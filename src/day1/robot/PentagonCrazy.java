471fe98a41ea7f880812c7d04e23fd3c85f80768
league-workshopper <workshop@Administrators-Mac-mini.local>
2015-10-07 15:00:18 -0700

    wtgawjheyrdfh

--- src/day1/robot/PentagonCrazy.java
+++ src/day1/robot/PentagonCrazy.java
@@ -10,27 +10,35 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+		Robot L = new Robot();
 		// 3. Put the robot's pen down
-
+		L.penDown();
 		// 8. Make the robot go at maximum speed (10)
-
+		L.setSpeed(10);
 		// 9. Set the pen to a color that you like for the shape
-
+		L.setRandomPenColor();
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+		int sides = 10; 
+		
 		// 5. Make a variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6.
-
+		int angle = 360/sides;
+		
 		// 7. Do steps #2 to #11, 200 times. When this is done you should see a pentagon.
+			for (int i = 0; i < 20000; i++) {
+				
+				
 
+			
 			// 2. Move the robot 200 pixels
-	
+			L.move(i);
 			// 10. Make the robot move "i" pixels instead of 200 (don’t need new line of code for this, just change previous one)
-	
+			
 			// 6. Turn the robot the amount in your angle variable
-	
-			// 11. Turn the robot one more degree
-
+					L.turn(angle);	
+					// 11. Turn the robot one more degree
+					L.turn(1);
+}
+			
 	}
 
 	// Variations:
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -5,23 +5,18 @@ import org.jointheleague.graphical.robot.Robot;
 public class Spiral {
 
 	public static void main(String[] args) {
-		// 1. Create a new Robot
 		
-		// 5. Set your robot's pen to the down position
-
-		// 3. Set the robot to go at max speed (10)
-
+		Robot K = new Robot();
+		
+		K.penDown();
+		
+		K.setSpeed(10);
 		// 4. Do the following (steps 7-9) 75 times
-
-			// 7. Change the pen color to random
-	
-			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-	
-			// 2. Turn the robot 1/3 of 360 degrees to the right
-	
-			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
-	
-			// 9. Set the pen width to i
-
+		for (int i = 0; i < 75; i++) {
+			K.setRandomPenColor();
+			K.move(15*i);
+			K.setPenWidth(i);
+			K.turn(360/11);
+}
 	}
 }
