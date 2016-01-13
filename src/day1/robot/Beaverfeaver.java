92d22a9b11277af2462f88746ad74ac9680b2771
thomberg <thomas.berg@e3civichigh.com>
2016-01-13 14:56:25 -0800

    hsdfsdhjsdgugug
    uhsygdfguhiuhfg

---
+++ src/day1/robot/Beaverfeaver.java
@@ -0,0 +1,22 @@
+package day1.robot;
+
+public class Beaverfeaver {
+	public static void main(String[] args) {
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.println("baby");
+			}
+			
+			
+		
+		if (i == 1) {
+				System.out.println("NO");
+			} 
+			else {
+
+				System.out.println("OOOOOOOOOOOOOOOHHHHHHHHHHHHHHHH");
+			}
+}
+		
+	}
+}
---
+++ src/day1/robot/bananaforever.java
@@ -0,0 +1,5 @@
+package day1.robot;
+
+public class bananaforever {
+
+}
