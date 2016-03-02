3b6fe3a6e08ec68582402422ade9248f842f7642
thomberg <league@Mac-mini-3.local>
2016-03-02 14:55:59 -0800

    gyudfsghguydfsgyuguyguyads
    eufygwqfugweuyfgweujyfgewjuyfgweuyfgtwe4uyfgwe

---
+++ src/badgers.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class badgers {
+	public static void main(String[] args) {
+		for (int i = 0; i < 12; i++) {
+			JOptionPane. showMessageDialog(null, "badgers");
+		}
+		for (int i = 0; i < 2; i++) {
+			JOptionPane. showMessageDialog(null, "mushroom");
+			
+		}
+	}
+}
---
+++ src/myages.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class myages {
+	public static void main(String[] args) {
+		for (int i = 0; i < 16; i++) {
+			JOptionPane.showMessageDialog(null,"I was " + i);
+			
+		}
+		
+	}
+}
